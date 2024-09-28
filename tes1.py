7615f00b0fff19d9a1519c1bdf9a608462a4455e
Ash <hilmy_ash13@student.uns.ac.id>
2024-09-28 15:25:17 +0700

    testttttttttt

---
+++ tes1.py
@@ -0,0 +1,13 @@
+def faktorial(n):
+    if n == 0 :
+        return 1
+    else :
+        return n*faktorial(n-1)
+    
+#input
+print(===========================)
+n= int(input("maukkan nilai faktorial =))
+print()
+
+#output
+print("nilai faktorial dari", n,"adalah =")
